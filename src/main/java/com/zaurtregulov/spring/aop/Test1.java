20d87b8841727608d918733d099af912cec722e4
Grigoriy Taran <grunder-96@mail.ru>
2022-12-21 13:12:53 +0300

    add lesson 5

--- src/main/java/com/zaurtregulov/spring/aop/Test1.java
+++ src/main/java/com/zaurtregulov/spring/aop/Test1.java
@@ -8,10 +8,10 @@ public class Test1 {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 		UniLibrary uniLibrary = context.getBean("uniLibrary", UniLibrary.class);
 //		SchoolLibrary schoolLibrary = context.getBean("schoolLibrary", SchoolLibrary.class);
-		Book book = context.getBean("book", Book.class);
-		uniLibrary.getBook(book);
+//		Book book = context.getBean("book", Book.class);
+		uniLibrary.getBook();
 //		uniLibrary.returnBook();
-//		uniLibrary.getMagazine();
+		uniLibrary.getMagazine();
 //		schoolLibrary.getBook();
 		context.close();
 	}
