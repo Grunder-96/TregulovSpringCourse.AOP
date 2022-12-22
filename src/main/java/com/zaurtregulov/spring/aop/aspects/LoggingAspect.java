ffa68033332af182cb4e94b5fe5b47a9dc808634
Grigoriy Taran <grunder-96@mail.ru>
2022-12-22 14:45:39 +0300

    add lesson 7

--- src/main/java/com/zaurtregulov/spring/aop/aspects/LoggingAspect.java
+++ src/main/java/com/zaurtregulov/spring/aop/aspects/LoggingAspect.java
@@ -2,26 +2,29 @@ package com.zaurtregulov.spring.aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-public class LoggingAndSecurityAspect {
+@Order(1)
+public class LoggingAspect {
 	
-	@Pointcut("execution(* com.zaurtregulov.spring.aop.UniLibrary.*())")
-	private void allMethodsFromUniLibrary() {}
 	
-	@Pointcut("execution(public void com.zaurtregulov.spring.aop.UniLibrary.returnMagazine())")
-	private void returnMagazineFromUniLibrary() {}
 	
-	@Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
-	private void allMethodsExceptReturnMagazineFromUniLibrary() {}
-	
-	@Before("allMethodsExceptReturnMagazineFromUniLibrary()")
-	public void beforeAllMethodsExceptReturnMagazineAdvice() {
-		System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: log #10");
-	}
+//	@Pointcut("execution(* com.zaurtregulov.spring.aop.UniLibrary.*())")
+//	private void allMethodsFromUniLibrary() {}
+//	
+//	@Pointcut("execution(public void com.zaurtregulov.spring.aop.UniLibrary.returnMagazine())")
+//	private void returnMagazineFromUniLibrary() {}
+//	
+//	@Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+//	private void allMethodsExceptReturnMagazineFromUniLibrary() {}
+//	
+//	@Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+//	public void beforeAllMethodsExceptReturnMagazineAdvice() {
+//		System.out.println("beforeAllMethodsExceptReturnMagazineAdvice: log #10");
+//	}
 	
 //	@Pointcut("execution(* com.zaurtregulov.spring.aop.UniLibrary.get*())")
 //	private void allGetMethodsFromUniLibrary() {}
@@ -51,10 +54,10 @@ public class LoggingAndSecurityAspect {
 //	@Pointcut("execution(* get*())")
 //	private void allGetMethods() {}
 //	
-//	@Before("allGetMethods()")
-//	public void beforeGetLoggingAdvice() {
-//		System.out.println("beforeGetLoggingAdvice: попытка получить книгу/журнал");
-//	}
+	@Before("com.zaurtregulov.spring.aop.aspects.MyPointcuts.allGetMethods()")
+	public void beforeGetLoggingAdvice() {
+		System.out.println("beforeGetLoggingAdvice: логирование" + " попытки получить книгу/журнал");
+	}
 //	
 //	@Before("allGetMethods()")
 //	public void beforeGetSecurityAdvice() {
